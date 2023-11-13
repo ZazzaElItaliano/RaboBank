74b1b7954624827690310af73c4e47e83123977a
Alejandro Martin Diaz <elprolol999@gmail.com>
2023-11-13 22:13:01 +0100

    subo cambios que no consigo que se suban

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="fotos\LOGODEF.svg" type="image/x-icon">
+    <title>BancoRabo-login</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <div class="wrapper">
+        <form action="submit">
+            <h1>Acceder</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Usuario">
+                <i class='bx bxs-user bx-tada' style='color:#00e3cc'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Contraseña">
+                <i class='bx bxs-lock bx-flip-vertical bx-tada' style='color:#00e3cc'  ></i>
+            </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </form>
+
+
+
+    </div>
+</body>
+</html>
