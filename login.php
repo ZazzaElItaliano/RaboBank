30f1bb5a04e5b7912968a0f8eda369370fd46692
Alejandro Martin Diaz <elprolol999@gmail.com>
2023-11-14 01:08:48 +0100

    subo parte del login y cambio nombre css

--- login.php
+++ login.php
@@ -5,24 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="fotos\LOGODEF.svg" type="image/x-icon">
     <title>BancoRabo-login</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilos.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
 <body>
-    <div class="wrapper">
+    <div class="login">
         <form action="submit">
-            <h1>Acceder</h1>
-            <div class="input-box">
+            <h1>Acceder al área cliente</h1>
+            <div class="caja-texto">
                 <input type="text" placeholder="Usuario">
                 <i class='bx bxs-user bx-tada' style='color:#00e3cc'></i>
             </div>
-            <div class="input-box">
+            <div class="caja-texto">
                 <input type="password" placeholder="Contraseña">
                 <i class='bx bxs-lock bx-flip-vertical bx-tada' style='color:#00e3cc'  ></i>
             </div>
+
+            <div class="Recuerda-password">
+               <label for="">
+                <input type="checkbox" name="" id="">
+                Recuerdame la contraseña
+               </label>
+               <a href="#">¿Olvidaste la contraseña?</a>
+            </div>
         
+            <button type="submit" class="boton">Acceder</button>
         
+            <div class="registro">
+
+            <p>¿Eres nuevo? <a href="registros.php" >Regístrate</a></p>
+
+            </div>
         
         
         
