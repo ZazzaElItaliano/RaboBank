fd60c1aa245a1d2408bb7fc22392a972566a8318
Alejandro Martin <alextoroviejo@gmail.com>
2023-11-28 13:59:25 +0100

    subo una prueba para el home

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos2.css">
+</head>
+<body>
+
+    <main>
+    <section class="red">
+        <p>holaaa</p>
+    </section>
+    <section class="blue">
+        <p>perenne</p>
+    </section>
+    <section class="yellow">
+        <p>perenne</p>
+    </section>
+    </main>
+</body>
+</html>
