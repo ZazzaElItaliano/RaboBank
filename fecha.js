490bb20b9c8c46c2e69f559dc978022f150e953e
Alejandro Martin <alextoroviejo@gmail.com>
2023-12-01 14:19:46 +0100

    subo fecha arreglada

--- fecha.js
+++ fecha.js
@@ -1,6 +1,12 @@
 let usarh2=document.getElementById("fechaHora");
 let tiempo= new Date();
-let fecha= tiempo.toLocaleDateString();
+var opcionesFecha = { weekday: 'long', 
+                      year: 'numeric', 
+                      month: 'long', 
+                      day: 'numeric' };
+
+
+let fecha= tiempo.toLocaleDateString("es-ES" , opcionesFecha);
 let hora= tiempo.toLocaleTimeString();
 let fechahora= "La fecha es: " + fecha + " y la hora es: " + hora;
 usarh2.innerHTML=fechahora;
